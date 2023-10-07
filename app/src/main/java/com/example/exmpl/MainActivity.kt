bd74b70b7d79cd4fb50832122677a17c0b2f4883
Jogender <jojosehrawat21@gmail.com>
2023-10-07 17:49:38 +0530

    Part 1 final commit

---
+++ app/src/main/java/com/example/exmpl/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.exmpl
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var vid:VideoView
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+    private lateinit var seekbar: SeekBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    // Video Player
+        vid = findViewById(R.id.vid)
+        vid.setVideoPath("android.resource://" + packageName + "/" + R.raw.vid1)
+        vid.start()
+
+        seekbar = findViewById(R.id.vidSeekBar)
+        handler = Handler(Looper.getMainLooper())
+        seekbar.progress = 0
+
+        vid.setOnPreparedListener{
+            seekbar.max = vid.duration
+            vid.start()
+            seekbar.progress = vid.currentPosition
+            changeSeekbar(vid)
+        }
+
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                if(changed){
+                    vid.seekTo(pos)
+                }
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+        val playPause = findViewById<ImageButton>(R.id.playPause)
+        playPause.setOnClickListener{
+            if(vid.isPlaying){
+                vid.pause()
+                playPause.setImageResource(R.drawable.baseline_play_arrow_24)
+            } else{
+                vid.start()
+                playPause.setImageResource(R.drawable.baseline_pause_24)
+                changeSeekbar(vid)
+            }
+        }
+
+        vid.setOnCompletionListener {
+            playPause.setImageResource(R.drawable.baseline_play_arrow_24)
+        }
+
+
+    // PDF File Open
+        val showHandout = findViewById<Button>(R.id.showHandout)
+        showHandout.setOnClickListener {
+            val intent = Intent(this, PdfViewerActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun changeSeekbar(videoView: VideoView) {
+        seekbar.progress = videoView.currentPosition
+        if (videoView.isPlaying) {
+            runnable = Runnable {
+                changeSeekbar(videoView)
+            }
+            handler.postDelayed(runnable, 100)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/exmpl/PdfViewerActivity.kt
@@ -0,0 +1,27 @@
+package com.example.exmpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.util.FitPolicy
+
+
+class PdfViewerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_viewer)
+
+        val pdfView = findViewById<PDFView>(R.id.pdfView)
+        pdfView.fromAsset("sample.pdf")
+            .enableSwipe(true)
+            .swipeHorizontal(false)
+            .enableDoubletap(true)
+            .defaultPage(0)
+            .pageFitPolicy(FitPolicy.WIDTH)
+            .spacing(100)
+            .pageFling(false)
+            .pageSnap(true)
+            .load()
+    }
+}
